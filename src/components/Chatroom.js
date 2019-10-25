a7a74799c0ef67629bd910ebfe11616f4166b662
Evelina <evelinawahlstrom18@gmail.com>
2019-10-25 12:38:10 +0200

    Chatform-connected-backend

---
+++ src/components/Chatroom.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import {url} from '../constants'
+
+export default class Chatroom extends Component {
+    state = {
+        messages: []
+    }
+    source = new EventSource (`${url}/stream`)
+
+
+    componentDidMount() {
+        console.log("componenDidMount of chatroom")
+        this.source.onmessage = event => {
+                console.log("Got a message!", event)
+                // data comes from the console.log, finding data in browser
+                const messages = JSON.parse(event.data)
+                console.log(JSON.parse(event.data), "event data?")
+                console.log(messages, "messages?")
+                this.setState({messages
+                })
+        }
+        console.log("source", this.source)
+    }
+    render() {
+        console.log("local state", this.state)
+        return (
+            <div>
+             <ul> 
+             {
+                 this.state.messages.map(message => <li key={message.id}> {message.message} </li>)
+             }
+             </ul>
+            </div>
+        )
+    }
+}
